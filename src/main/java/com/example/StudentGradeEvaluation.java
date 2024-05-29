9cacb3f1b502b009561c4500884efda702854fac
venkatganesh1998 <venkat.ganesh1998@gmail.com>
2024-05-29 08:52:01 +0530

    finish

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -5,20 +5,41 @@ import java.util.Scanner;
 public class StudentGradeEvaluation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String name;
+        int age,score;
+        
         // Prompt the user to enter their name
         System.out.print("Enter your name: ");
-        
+        name=scanner.nextLine();
 
         // Prompt the user to enter their age
-        
-
+        System.out.print("Enter your age: ");
+        age=scanner.nextIntInt();
         // Prompt the user to enter their exam score
-        
-
+        System.out.print("Enter your score: ");
+        score=scanner.nextIntInt();
         // Determine the grade
         String grade;
-        
+        if(score>=90){
+
+        }
+        elseif(score>=80){
+            System.out.print("A ");
+            if(age<25)
+            System.out.print("eligible ");
+        }
+        elseif(score>=70){
+            System.out.print("B ");
+            if(age<25)
+            System.out.print("eligible ");
+            
+        }
+        elseif(score>=60){
+            System.out.print("C ");
+        }
+        elseif(score<80){
+            System.out.print("D");
+        }
 
         // Print the student's details
         
